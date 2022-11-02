ed51cd7e0d8d499c8d68f301c1251ff7a42a8709
Alastair <alastair.chau@gmail.com>
2022-11-02 14:08:02 +0000

    code update for webservice

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -11,6 +11,14 @@ class DatabaseConnection
   # PG gem. We connect to 127.0.0.1, and select
   # the database name given in argument.
   def self.connect
+  # NEW DATABASE setup for Render webservice
+  # If the environment variable (set by Render)
+  # is present, use this to open the connection.
+    if ENV['DATABASE_URL'] != nil
+      @connection = PG.connect(ENV['DATABASE_URL'])
+      return
+    end
+
     if ENV['ENV'] == 'test'
       database_name = 'music_library_test'
     else
